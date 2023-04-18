10b933900b006e7de976dded66038d41b7ddb4ce
JDavidCarreno <carrenojuann97@gmail.com>
2023-04-18 10:59:40 -0500

    change on index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+// import ReactDOM from 'react-dom'
+import * as ReactDOMClient from 'react-dom/client';
 import './index.css'
 import App from './App'
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-)
+// ReactDOM.render(
+//   <App />,
+//   document.getElementById('root')
+// )
+
+const container = document.getElementById('root');
+const root = ReactDOMClient.createRoot(container);
+root.render(<App />);
