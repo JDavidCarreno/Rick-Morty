e0c53a34bf671df2a437c41817994ddcb51e97cf
JDavidCarreno <carrenojuann97@gmail.com>
2023-05-04 10:40:18 -0500

    promises

--- Server/src/controllers/getCharById.js
+++ Server/src/controllers/getCharById.js
@@ -2,20 +2,33 @@ const axios = require('axios')
 
 const getCharById = (res, id) => {
     axios(`https://rickandmortyapi.com/api/character/${id}`)
-    .then((response) => { 
-        const { name, gender, species, origin, image, status} = response.data;
-        let character = {
-            id,
-            name,
-            gender,
-            species,
-            origin,
-            image,
-            status
-        };
+    .then(result => result.data)
+    .then((data) => {
+        const character = {
+            id: data.id,
+            name: data.name,
+            gender: data.gender,
+            origin: data.origin,
+            species: data.species,
+            image: data.image,
+            status: data.status
+        }
         res.writeHead(200, {"Content-type": "application/json"});
         res.end(JSON.stringify(character));
     })
+    // .then((response) => { 
+    //     const { name, gender, species, origin, image, status} = response.data;
+    //     let character = {
+    //         id,
+    //         name,
+    //         gender,
+    //         species,
+    //         origin,
+    //         image,
+    //         status
+    //     };
+    //     res.writeHead(200, {"Content-type": "application/json"});
+    //     res.end(JSON.stringify(character))})
     .catch((error) => {
         res.writeHead(500, {"Content-type": "text/plain"});
         res.end(error.message)
--- Server/src/index.js
+++ Server/src/index.js
@@ -1,6 +1,5 @@
-// const data = require('./utils/data');
 
-const getCharById = require('./controllers/getCharById');
+const { getCharById } = require('./controllers/getCharById');
 
 const http = require('http');
 const PORT = 3001;
@@ -9,10 +8,9 @@ const PORT = 3001;
 http.createServer((req, res) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     if (req.url.includes("/rickandmorty/character")) {
-        let id = Number(req.url.slice(-1));
-        // let character = data[id - 1];
-        // res.writeHead(200, {"Content-type": "application/json"});
-        // res.end(JSON.stringify(character))
+        // let id = Number(req.url.slice(-1)); //split('/').at(-1)
+        let id = req.url.split('/').at(-1)
+
         getCharById(res, id)
     }
 
